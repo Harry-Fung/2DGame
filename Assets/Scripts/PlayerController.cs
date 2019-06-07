c74b4c5005cc8f2a1f51c68ca206c8566f94aee5
Xiaojun Feng <xiaojunfeng1@gmail.com>
2019-06-07 00:00:31 -0700

    changed player image, added dropping egg function, added a fire button but it's not functional yet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -95,5 +95,21 @@ public class PlayerController : MonoBehaviour
 
         //CheckCapsuleEndCollisions();
         //CheckCapsuleEndCollisions(false);
+
+        bool shoot = Input.GetButtonDown("Fire1");
+        //shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+
+
     }
 }
---
+++ Assets/Scripts/egg1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class egg1 : MonoBehaviour
+{
+    //BowlingBall
+    public GameObject eggs;
+    private float time =2;
+    private GameObject newegg;
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            Vector3 player_postion = GameObject.Find("screamingChicken1").transform.position;
+            float eggx = player_postion.x;
+            time = 2;
+            
+            Debug.Log(eggx);
+            Vector3 spawnPosition = new Vector3(eggx, 3, 0);
+            newegg = (GameObject)Instantiate(eggs, spawnPosition, Quaternion.identity);
+            newegg.name = "newegg";
+            (gameObject.GetComponent("egg1") as MonoBehaviour).enabled = false;
+            //Destroy(newegg, 3);
+        }
+    }
+}
